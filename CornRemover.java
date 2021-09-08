8c86719d5a2cef8372872a5a2efd866c295a3638
IlyaShch <57571604+IlyaShch@users.noreply.github.com>
2021-09-07 20:33:45 -0400

    Update CornRemover.java

--- CornRemover.java
+++ CornRemover.java
@@ -6,9 +6,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 
-// 
-// Decompiled by Procyon v0.5.36
-// 
 
 public class CornRemover
 {
